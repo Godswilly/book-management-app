fc6f75a48e39e1d73506a66061c58104529bb5c1
Kalu Agu Kalu <kalu.agukalu@gmail.com>
2021-09-24 13:13:55 +0100

    Defined state and check if props are passed or not

---
+++ src/components/BookForm.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import { Form, Button } from 'react-bootstrap';
+import { v4 as uuidv4 } from 'uuid';
+
+const BookForm = (props) => {
+  const [book ,setBook] = useState({
+    bookName: props.bookName ? props.book.bookName : '',
+    author: props.author ? props.book.author : '',
+    quantity: props.quantity ? props.book.quantity : '',
+    price: props.price ? props.book.price : '',
+    date: props.date ? props.book.date : '',
+  });
+}
+
+export default BookForm;
